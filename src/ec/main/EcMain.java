f4b766ab85be00c2ba5cf7b5fde7e50ee49bc7a6
sri303030 <sri303030@gmail.com>
2015-11-17 13:20:18 -0600

    log-in creation

--- src/ec/main/EcMain.java
+++ src/ec/main/EcMain.java
@@ -1,6 +1,10 @@
 package ec.main;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
+import org.json.simple.JSONObject;
 
 import ec.util.EcPropertyValues;
 
@@ -15,6 +19,7 @@ public class EcMain {
 	public static void main(String[] args) throws IOException {
 		
 		long startTime = System.currentTimeMillis();
+		JSONObject obj = new JSONObject();
 		
 		EcPopulation pop = new EcPopulation();
 		EcTree targetFunction = new EcTree();
@@ -22,7 +27,9 @@ public class EcMain {
 		boolean targetFitnessReached = false;
 		final Double INPUT[] = {-3.0,-2.0,-1.0,0.0,1.0,2.0,3.0};
 		final Double OUTPUT[] = {4.0,1.5,0.0,-0.5,0.0,1.5,4.0} ;
-		
+		FileWriter file = new FileWriter("/login.txt",true);
+		List <Double> fitness= new ArrayList<Double>();
+		long startSelection=0,stopSelection=0,startCrossOver=0,stopCrossOver=0,startMutation=0,stopMutation = 0;
 		//initialize the population
 		fillUpPopulation(pop.getCurrentPopulation());
 		
@@ -39,11 +46,22 @@ public class EcMain {
 			if (targetFitnessReached) {
 				break;
 			}
+			startSelection = System.nanoTime();
 			pop.doSelection();
+			stopSelection += System.nanoTime() - startSelection;
+			obj.put("Time taken for selection",stopSelection/1000000000.0);
+			//double fitnessValue=pop.getNextPopulation().get(0).getFitness();
 			System.out.println("Top Fitness Value: " + pop.getNextPopulation().get(0).getFitness());
+			fitness.add(pop.getNextPopulation().get(0).getFitness());
 			EcTree clone = new EcTree(pop.getNextPopulation().get(0).getRoot().clone()); //Clone the most fit individual
+			startCrossOver = System.nanoTime();
 			pop.doCrossover();
+			stopCrossOver += System.nanoTime() - startCrossOver;
+			obj.put("Time taken for Crossover",stopCrossOver/1000000000.0 );
+			startMutation = System.nanoTime();
 			pop.doMutation();
+			stopMutation += System.nanoTime() - startMutation;
+			obj.put("Time taken for Mutation",stopMutation/1000000000.0);
 			pop.getNextPopulation().add(clone); //add the clone to the next population
 			fillUpPopulation(pop.getNextPopulation());
 			pop.setCurrentPopulation(pop.getNextPopulation());
@@ -52,7 +70,13 @@ public class EcMain {
 		long stopTime = System.currentTimeMillis();
 		System.out.println("THIS IS THE TARGET FUNCTION");
 		targetFunction.displayTree();
+		obj.put("Worst Fitness",fitness.get(0));
+		obj.put("Best Fitness",fitness.get(fitness.size()-1));
+		
 		System.out.println(" Total elapsed time: " +  (stopTime - startTime ) / 1000 + " seconds" );
+		obj.put("Total elapsed time", (stopTime - startTime ) / 1000);
+		file.write(obj.toJSONString());
+		file.close();
 		
 	}
 	
